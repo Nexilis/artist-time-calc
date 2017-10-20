3894991c23006c30d6cabf7b009985705fc1f567
Bartek Łukasik <Nexilis@users.noreply.github.com>
2017-10-20 23:11:52 +0200

    First lines of code...

--- src/artist_time_calc/core.clj
+++ src/artist_time_calc/core.clj
@@ -1,7 +1,10 @@
 (ns artist-time-calc.core
   (:gen-class))
 
+(def config {:days 20 :factor 0.7})
+(def copyrighted-hours (reduce * (vals config)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println copyrighted-hours))
