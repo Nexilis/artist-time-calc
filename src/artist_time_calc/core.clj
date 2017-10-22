8fc3e72c39460e87c1af29f34d6f13b75793016a
Bartek Łukasik <Nexilis@users.noreply.github.com>
2017-10-22 14:41:14 +0200

    Working implementation.

--- src/artist_time_calc/core.clj
+++ src/artist_time_calc/core.clj
@@ -6,21 +6,33 @@
 ; cr - copyrighted
 ; h  - hours
 
-(def config {:wr-days 20 :wr-h-per-day 8 :cr-percentage 0.7})
-(def cr-total-h (Math/floor (reduce * (vals config))))
-(def cr-base-h-per-day (Math/floor (/ cr-total-h (config :wr-days))))
-(def cr-surplus-h (mod cr-total-h (config :wr-days)))
+(def config {:wr-days 20 :wr-h-per-day 8 :cr-percentage 0.65})
+(def cr-total-h (int (Math/floor (reduce * (vals config)))))
+(def cr-base-h-per-day (int (Math/floor (/ cr-total-h (config :wr-days)))))
+(def cr-surplus-h (int (mod cr-total-h (config :wr-days))))
 (def wr-total-h (* (config :wr-days) (config :wr-h-per-day)))
 
-; [{ :day :wr-h x :cr-h (- (config :wr-h-per-day) x)}]
-(defn create-wr-calendar []
-  (let [calendar []]
-    (conj calendar {:day 1 :cr-h 0 :wr-h 0})))
+(defn calc-cr-h-this-day [remaining-surplus-h]
+  (+ cr-base-h-per-day
+     (if (> remaining-surplus-h 0) 1 0)))
+
+(defn calc-calendar []
+  (loop [i 1
+         calendar []
+         remaining-surplus-h cr-surplus-h]
+    (let [cr-h-this-day (calc-cr-h-this-day remaining-surplus-h)]
+      (if (<= i (config :wr-days))
+        (recur (inc i)
+               (conj calendar {:day i
+                               :cr-h cr-h-this-day
+                               :wr-h (- (config :wr-h-per-day) cr-h-this-day)})
+               (dec remaining-surplus-h))
+        calendar))))
 
 (defn -main
   [& args]
+  (print-table (calc-calendar))
   (println "Total worked hours:" wr-total-h)
   (println "Total copyrighted hours:" cr-total-h)
   (println "Base copyrighted hours per day:" cr-base-h-per-day)
-  (println "Surplus copyrighted hours:" cr-surplus-h)
-  (print-table (create-wr-calendar)))
+  (println "Surplus copyrighted hours:" cr-surplus-h))
