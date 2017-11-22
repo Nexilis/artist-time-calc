48dfe6b3abc5a23848da0e81688ae86c23648d08
Bartek Łukasik <Nexilis@users.noreply.github.com>
2017-11-22 21:18:48 +0100

    Working randomization.
    Fixed configuration issues.

--- src/artist_time_calc/core.clj
+++ src/artist_time_calc/core.clj
@@ -6,9 +6,14 @@
 ; cr - copyrighted
 ; h  - hours
 
-(def config {:wr-days 20 :wr-h-per-day 8 :cr-percentage 0.7})
-(def randomization-lvl 20)
-(def cr-total-h (int (Math/floor (reduce * (vals config)))))
+(def config {:wr-days 20
+             :wr-h-per-day 8
+             :cr-percentage 0.7
+             :randomization-lvl 20})
+
+(def cr-total-h (int (Math/floor (* (config :wr-days)
+                                    (config :wr-h-per-day)
+                                    (config :cr-percentage)))))
 (def cr-base-h-per-day (int (Math/floor (/ cr-total-h (config :wr-days)))))
 (def cr-surplus-h (int (mod cr-total-h (config :wr-days))))
 (def wr-total-h (* (config :wr-days) (config :wr-h-per-day)))
@@ -32,14 +37,25 @@
 
 (defn two-various-days [calendar]
   (let [fir-day (rand-nth calendar)
-        cal-removed-fir-day (remove (fn [x] (= (x :day) (fir-day :day))) calendar)
+        cal-removed-fir-day (remove
+                              (fn [x] (= (x :day) (fir-day :day)))
+                              calendar)
         sec-day (rand-nth cal-removed-fir-day)]
     (vector fir-day sec-day)))
+;; TODO: instead create vector from [0...:wr-days-1] and rand-nth two indexes
 
 (defn single-calendar-randomization
-  [calendar days-to-unbalance])
-;; TODO: try to unbalance two days (subtract hour from first and add to second)
-;; TODO: return new calendar, remember about sorting by :day
+  [calendar days-to-unbalance]
+  (let [fir-day (get days-to-unbalance 0)
+        sec-day (get days-to-unbalance 1)]
+    (update-in
+      (update-in
+        (update-in
+          (update-in calendar [(- (fir-day :day) 1) :wr-h] inc)
+          [(- (fir-day :day) 1) :cr-h] dec)
+        [(- (sec-day :day) 1) :wr-h] dec)
+      [(- (sec-day :day) 1) :cr-h] inc)))
+;; TODO: prevent randomization to create negative values
 
 (defn randomize-calendar
   "Randomizes passed calendar according to randomization lvl"
@@ -47,7 +63,7 @@
   (loop [i 1
          days-to-unbalance (two-various-days calendar)
          rand-calendar calendar]
-    (if (= i randomization-lvl)
+    (if (= i (config :randomization-lvl))
       rand-calendar
       (recur (inc i)
              (two-various-days rand-calendar)
@@ -55,7 +71,8 @@
 
 (defn -main
   [& args]
-  (print-table (calc-calendar))
+  ;; TODO: add unit tests summing cr-h and comparing with cr-total-h, etc
+  (print-table (randomize-calendar (calc-calendar)))
   (println "Total worked hours:" wr-total-h)
   (println "Total copyrighted hours:" cr-total-h)
   (println "Base copyrighted hours per day:" cr-base-h-per-day)
