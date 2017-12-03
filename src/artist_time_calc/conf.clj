7004cf4347d13ab2181a544693d5393b80d8181d
Bartek Łukasik <Nexilis@users.noreply.github.com>
2017-12-03 12:07:00 +0100

    Config values calculation consolidated in a method.

--- src/artist_time_calc/conf.clj
+++ src/artist_time_calc/conf.clj
@@ -10,6 +10,14 @@
              :cr-percentage 0.7
              :randomization-lvl 10})
 
+(defn calculate-config [conf]
+  (let [conf (conj conf {:cr-total-h (int (Math/floor (* (conf :wr-days) (conf :wr-h-per-day) (conf :cr-percentage))))})]
+    (conj conf {:cr-base-h-per-day (int (Math/floor (/ (conf :cr-total-h) (conf :wr-days))))}
+               {:cr-surplus-h (int (mod (conf :cr-total-h) (config :wr-days)))}
+               {:wr-total-h (* (config :wr-days) (config :wr-h-per-day))}
+      conf)))
+
+
 (def cr-total-h (int (Math/floor (* (config :wr-days)
                                     (config :wr-h-per-day)
                                     (config :cr-percentage)))))
